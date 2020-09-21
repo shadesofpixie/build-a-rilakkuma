a4ce2d1834ceeb6c4e6c0ebc73a53b2f82665089
Kevin <Atnnetworkk@gmail.com>
2020-09-21 21:37:21 +0200

    Add basic drag & drop vanilla

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
-$( function() {
-    $( ".draggable" ).draggable();
-  } );
+let dragablesItems = document.querySelectorAll(".draggable");
+let currentItem;
+const moveItem = (x, y, elt) => {
+  elt.style.left = `${x - elt.offsetWidth / 2}px`;
+  elt.style.top = `${y - elt.offsetHeight / 2}px`;
+  return null;
+};
+
+//Event function
+
+const handleMove = (e) => {
+  console.log(e.target);
+  moveItem(e.pageX, e.pageY, currentItem);
+};
+
+//Manual Drag & Drop
+
+dragablesItems.forEach((item) => {
+  item.addEventListener("dragstart", (e) => {
+    return false;
+  });
+  item.addEventListener("mousedown", () => {
+    currentItem = item;
+    document.addEventListener("mousemove", handleMove);
+  });
+
+  item.addEventListener("mouseup", () => {
+    currentItem = null;
+    document.removeEventListener("mousemove", handleMove);
+    item.removeEventListener("mouseup", handleMove);
+  });
+});
